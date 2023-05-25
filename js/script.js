bb6309490500ef8755a9b75fe66fc4c4b3c31407
05onwi <williamdavidjan@gmail.com>
2023-05-25 11:44:13 +0200

    albin

--- js/script.js
+++ js/script.js
@@ -25,23 +25,44 @@ function buttonClick(e) {
     // kollar om siffertangent är nedtryckt
     if (btn.substring(0, 1) === 'b') {
         let digit = btn.substring(1, 2); // plockar ut siffran från id:et
-
-    } else { // Inte en siffertangent, övriga tangenter.
-
+        addDigit(digit);
+    } 
+    else if(btn === 'clear'){
+        memClear();
     }
+    else if(btn === 'comma'){
+        addComma();
+    }
+    else if(btn === 'add'){
+        setOperator('+');
+    }
+    else if(btn === 'sub'){
+        setOperator('-');
+    }
+    else if(btn === 'mul'){
+        setOperator('*');
+    }
+    else if(btn === 'div'){
+        setOperator('/');
+    }
+    else if(btn === 'enter'){
+        calculate(memory, lcd.value);
+    }
+   
 }
-
 /**
  *  Lägger till siffra på display.
  */
 function addDigit(digit) {
+    lcd.value += digit;
 }
+
 
 /**
  * Lägger till decimaltecken
  */
 function addComma() {
-
+    lcd.value += '.';
 }
 
 /**
@@ -49,14 +70,27 @@ function addComma() {
  * +, -, *, /
  */
 function setOperator(operator){
-
+    arithmetic = operator;
+    memory = lcd.value;
+    clearLCD();
 }
 
 /**
  * Beräknar ovh visar resultatet på displayen.
  */
-function calculate() {
-
+function calculate(i,j) {
+   if(arithmetic == '+'){
+    lcd.value =  parseFloat(i) +  parseFloat(j);
+   }
+  else  if(arithmetic == '-'){
+    lcd.value = i - j;
+   }
+  else  if(arithmetic == '/'){
+    lcd.value = i / j;
+   }
+  else if(arithmetic == '*'){
+    lcd.value = i * j;
+   }
 }
 
 /** Rensar display */
