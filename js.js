0cd722ef1730e24268272f708cc6907e67f46883
William <williamdavidjan@gmail.com>
2023-08-31 15:27:07 +0200

    test

--- js.js
+++ js.js
@@ -1,31 +1,28 @@
 let count = 0;
 
 function init(){
-    document.getElementById("liksom").onclick = click;
+    let liksom = document.getElementById("liksom");
+    liksom.onmousedown = click;
+    liksom.onmouseup = unclick;
     
    
 }
  
 function click(){
+   
+   
     count++;
     document.getElementById("klick").innerHTML = count;
-    setRandomColor;
+    liksom.style.height = "49rem";
+    liksom.style.width = "49rem";
+}
+
+function unclick(){
+    liksom.setAttribute("style","width:50rem");
+    liksom.setAttribute("style","height:50rem");
+
+
 }
 
 window.onload = init;
 
-
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-  
-  
-  
-  function setRandomColor() {
-    $("#liksom").css("background-color", getRandomColor());
-  }
