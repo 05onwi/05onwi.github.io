185e05be389ecb337ff53ab069dd23cdd1619d86
05onwi <williamdavidjan@gmail.com>
2023-08-31 21:32:30 +0200

    test

--- js.js
+++ js.js
@@ -1,28 +1,31 @@
 let count = 0;
+let color = null;
 
 function init(){
     let liksom = document.getElementById("liksom");
     liksom.onmousedown = click;
     liksom.onmouseup = unclick;
-    
-   
 }
  
 function click(){
-   
-   
     count++;
     document.getElementById("klick").innerHTML = count;
     liksom.style.height = "49rem";
     liksom.style.width = "49rem";
+    färg();
+    liksom.style.background = color;
 }
 
 function unclick(){
-    liksom.setAttribute("style","width:50rem");
-    liksom.setAttribute("style","height:50rem");
-
-
+    liksom.style.height = "50rem";
+    liksom.style.width = "50rem";
+    
 }
 
-window.onload = init;
+function färg(){
+    const färger = [ "Red", "Green", "Blue", "Yellow", "Violet", "Lime", "Pink", "Aqua", "Indigo", "Orange", "orangered", "Aquamarine", "Skyblue", 
+"Darkblue", "Fuchsia", "Gld"];
+    return color = färger[Math.round(Math.random()*15)];
+  }
 
+window.onload = init;
